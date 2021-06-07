ecf917062607b61a34b4875983ffb24b73c44008
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-06 22:11:25 -0500

    add renderComments

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -4,4 +4,32 @@ class Comment {
         this.book = book_id
         this.user = user_id
     }
+
+    static fetchComments = (book) => {
+        console.log(book)
+        // let comments
+        fetch(`http://localhost:3000/books/${book}/comments`)
+            .then(response => response.json())
+            .then(comments => comments.forEach(comment => {
+                if (comment.book_id === book){
+                    comment = new Comment(comment.content, comment.book_id, comment.user_id)
+                    Comment.renderComments(comment)
+                }
+            }))
+            // console.log(comment)
+            // renderComments(book, comments)
+    }
+
+    static renderComments(comment) {
+        console.log(comment)
+        let bookCard = document.getElementById(book)
+        // let commentArea = document.createElement('div')
+        // bookCard.append(commentArea)
+        // let ul = document.createElement('ul')
+        // commentArea.append(ul)
+        // console.log(comments.length)
+        // for(let i =0; i < comments.length; i++){
+        //     console.log(comments)
+        // }
+    }
 }
