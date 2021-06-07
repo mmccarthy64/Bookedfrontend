aa8900e1f7735089ab66a6bb323cab8dacb5ae41
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-06 20:34:59 -0500

    add getbooks and renderbooks to book class

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     getBooks();
 })
 
-function getBooks(){
-    return fetch('http://localhost:3000/books')
-        .then(resp => resp.json())
-        .then(books => books.forEach(book => renderBooks(book)))
-}
+// function getBooks(){
+//     return fetch('http://localhost:3000/books')
+//         .then(resp => resp.json())
+//         .then(books => books.forEach(book => renderBooks(book)))
+// }
 
 // const newBook = document.getElementById('new-book-btn').addEventListener('click', postBook)
 // const newBookForm = document.querySelector('#new-book-form')
@@ -37,44 +37,44 @@ async function postBook(new_book, image){
     })
 }
 
-function renderBooks(book) {
-    const booksContainer = document.getElementById('books-container')
-    let h2 = document.createElement('h2')
-    h2.innerText = book.title
+// function renderBooks(book) {
+//     const booksContainer = document.getElementById('books-container')
+//     let h2 = document.createElement('h2')
+//     h2.innerText = book.title
 
-    let p = document.createElement('p')
-    p.innerText = book.author
+//     let p = document.createElement('p')
+//     p.innerText = book.author
 
-    let img = document.createElement('img')
-    if(book.img != ""){
-        img.setAttribute('src', book['img'])
-        img.setAttribute('class', 'book-image')
-    }
+//     let img = document.createElement('img')
+//     if(book.img != ""){
+//         img.setAttribute('src', book['img'])
+//         img.setAttribute('class', 'book-image')
+//     }
 
-    let btn = document.createElement('button')
-    btn.innerText = "Delete from Library"
-    btn.addEventListener('click', (e) => {
-        e.preventDefault()
-        divCard.remove()
-        deleteBookFromBookshelf(book.id)
-    })
+//     let btn = document.createElement('button')
+//     btn.innerText = "Delete from Library"
+//     btn.addEventListener('click', (e) => {
+//         e.preventDefault()
+//         divCard.remove()
+//         deleteBookFromBookshelf(book.id)
+//     })
 
-    // let cbtn = document.createElement('button')
-    // cbtn.innerText = "View Comments"
-    // cbtn.addEventListener('click', (e) => {
-    //     e.preventDefault()
-    //     console.log(book)
-    //     fetchComments(book.id)
-    //     // console.log(comments)
-    // })
+//     // let cbtn = document.createElement('button')
+//     // cbtn.innerText = "View Comments"
+//     // cbtn.addEventListener('click', (e) => {
+//     //     e.preventDefault()
+//     //     console.log(book)
+//     //     fetchComments(book.id)
+//     //     // console.log(comments)
+//     // })
 
-    let divCard = document.createElement('div')
-    divCard.setAttribute('class', 'card')
-    divCard.setAttribute('id', book.id)
+//     let divCard = document.createElement('div')
+//     divCard.setAttribute('class', 'card')
+//     divCard.setAttribute('id', book.id)
 
-    divCard.append(h2, p, img, btn)
-    booksContainer.append(divCard)
-}
+//     divCard.append(h2, p, img, btn)
+//     booksContainer.append(divCard)
+// }
 
 async function googleBooksSearch(search) {
     return fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyBVOEuQ0f8FopsXl0HthBSJ1GIBIbI0C2Y`)
--- src/models/Book.js
+++ src/models/Book.js
@@ -7,7 +7,49 @@ class Book {
         this.renderBook = this.renderBook.bind(this)
     }
 
-    renderBook(){
+    getBooks = () => {
+        return fetch('http://localhost:3000/books')
+            .then(resp => resp.json())
+            .then(books => books.forEach(book => renderBooks(book)))
+    }
+
+    renderBooks = book => {
         console.log(this)
+        const booksContainer = document.getElementById('books-container')
+        let h2 = document.createElement('h2')
+        h2.innerText = book.title
+    
+        let p = document.createElement('p')
+        p.innerText = book.author
+    
+        let img = document.createElement('img')
+        if(book.img != ""){
+            img.setAttribute('src', book['img'])
+            img.setAttribute('class', 'book-image')
+        }
+    
+        let btn = document.createElement('button')
+        btn.innerText = "Delete from Library"
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            divCard.remove()
+            deleteBookFromBookshelf(book.id)
+        })
+    
+        // let cbtn = document.createElement('button')
+        // cbtn.innerText = "View Comments"
+        // cbtn.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     console.log(book)
+        //     fetchComments(book.id)
+        //     // console.log(comments)
+        // })
+    
+        let divCard = document.createElement('div')
+        divCard.setAttribute('class', 'card')
+        divCard.setAttribute('id', book.id)
+    
+        divCard.append(h2, p, img, btn)
+        booksContainer.append(divCard)
     }
 }
